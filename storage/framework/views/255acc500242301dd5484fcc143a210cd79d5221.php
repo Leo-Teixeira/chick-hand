6774f4e9347161607ab7e10b5abce1586343d0a2
hugoBRET <hugo.breteche49@gmail.com>
2021-12-10 16:35:28 +0100

    premierCommit

---
+++ storage/framework/views/255acc500242301dd5484fcc143a210cd79d5221.php
@@ -0,0 +1,13 @@
+ <?php $__env->startSection('title', 'Bienvenue'); ?> <?php $__env->startSection('content'); ?>
+    <div class="title m-b-md">Chick'hand</div>
+
+    <div class="links">
+        <a href="https://laravel.com/docs">Documentation</a>
+        <a href="https://laracasts.com">Laracasts</a>
+        <a href="https://laravel-news.com">News</a>
+        <a href="https://forge.laravel.com">Forge</a>
+        <a href="https://github.com/laravel/laravel">GitHub</a>
+    </div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.base', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\hbreteche\Documents\AP4\chick'hand\resources\views/welcome.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/8a2fdeb041f6012dc11b1110247ffbbe735331c3.php
@@ -0,0 +1,7 @@
+<?php $__env->startSection('title', 'Bienvenue'); ?>
+
+<?php $__env->startSection('content'); ?>
+    <h1>connexion</h1>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.base', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\hbreteche\Documents\AP4\chick'hand\resources\views/connexion.blade.php ENDPATH**/ ?>
